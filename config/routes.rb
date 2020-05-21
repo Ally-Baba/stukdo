a7e7b5fcbc9659c67a92b027f5cdccac70b547bf
Alexandria Irondina Keene <alexandria@Alexandrias-MacBook-Pro-2.local>
2020-05-21 13:03:25 -0500

    Updated panel

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
       put :change
     end
   end
-  
+
   devise_for :users
   get 'pages/home'
   get 'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get 'about' => 'pages#about'
+  get 'about' => "pages#about"
+  get 'test' => "pages#test"
 end
